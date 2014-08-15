776d59a2d1f3ae2ab5f9fadd2bb7cf507bfad718
Arnold <arnold@aztec.(none)>
2014-08-15 16:51:15 +0200

    Raw start

---
+++ src/Receiver.java
@@ -0,0 +1,19 @@
+import java.net.*;
+import java.util.HashSet;
+import java.io.*;
+
+/*
+ * Author: Timotius Johannes Hendrickes Gabriel Butler
+ * 
+ * Receiver program that receives data from the sender through UDP
+ * Datagram Packets or TCP packets. GENIET HOM TIM
+ */
+public class Receiver 
+{
+	
+	public static void main(String[] args)
+	{
+
+	}
+
+}
---
+++ src/Sender.java
@@ -0,0 +1,18 @@
+import java.net.*;
+import java.util.HashSet;
+import java.io.*;
+
+/*
+ * Author: Arnold Visser
+ * 
+ * Sender program that browses files and sends it to the receiver
+ * using UDP Datagram Packets or TCP
+ */
+public class Sender {
+
+	public static void main(String[] args)
+	{
+
+	}
+
+}
