bb7af647d4e0fa3cb482436728538c724d82db0d
Arnold <arnold@aztec.(none)>
2014-08-17 17:32:56 +0200

    need more farm

--- src/Receiver.java
+++ src/Receiver.java
@@ -1,19 +1,43 @@
 import java.net.*;
 import java.util.HashSet;
 import java.io.*;
+import java.nio.channels.*;
 
 /*
- * Author: Timotius Johannes Hendrickes Gabriel Butler
+ * Author: Timotius Johannes Petrus Gabriel Butler
  * 
  * Receiver program that receives data from the sender through UDP
  * Datagram Packets or TCP packets. GENIET HOM TIM
  */
 public class Receiver 
 {
+	//private static DatagramSocket socket;
 	
 	public static void main(String[] args)
 	{
-
+		try
+		{	
+			DatagramSocket socket = new DatagramSocket(2000);
+			byte[] sendArray = new byte[40779];
+	        // get response
+	        DatagramPacket packet = new DatagramPacket(sendArray, sendArray.length);
+	        socket.receive(packet);
+	        
+			RandomAccessFile newFile = new RandomAccessFile("firstSend.jpg", "rw");
+			try 
+			{
+				newFile.write(sendArray);
+				System.out.println("got it");
+				
+			} catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+	        socket.close();
+		}
+		catch (Exception e)
+		{
+			System.err.println("poes");
+		}
 	}
-
 }
--- src/Sender.java
+++ src/Sender.java
@@ -1,5 +1,5 @@
 import java.net.*;
-import java.util.HashSet;
+//import java.util.HashMap;
 import java.io.*;
 
 /*
@@ -8,11 +8,67 @@ import java.io.*;
  * Sender program that browses files and sends it to the receiver
  * using UDP Datagram Packets or TCP
  */
-public class Sender {
-
-	public static void main(String[] args)
+public class Sender extends Thread
+{
+	private DatagramSocket socket;
+	private byte[] sendArray;
+	//private byte[] receiveArray;
+	//private byte[] dataArray;
+	private final int CHUNKSIZE = 64000;
+	//private HashMap<Integer, int[]> SentData = new HashMap<Integer, int[]>();
+	//private boolean keepAlive = true;
+	
+	public void run()
 	{
-
+		int port = 6066;
+		String host = "localhost";
+		try 
+		{
+			RandomAccessFile selectedFile = null;
+			selectedFile = new RandomAccessFile("drow.jpg", "r");
+			
+			if ((int) selectedFile.length() > CHUNKSIZE)
+			{
+				System.err.println("FILE TOO BIG");
+				System.err.println(((int) selectedFile.length() / CHUNKSIZE)); //determine packets
+			}
+			sendArray = new byte[(int) selectedFile.length()];
+			
+			while (selectedFile.getFilePointer() < selectedFile.length())
+			{	
+				selectedFile.read(sendArray);
+			}
+			System.out.println(selectedFile.length());
+			
+			socket = new DatagramSocket(port);
+			
+			InetAddress address = InetAddress.getByName(host);
+	        DatagramPacket packet = new DatagramPacket(sendArray, sendArray.length, address, 2000); 
+	        socket.send(packet);			
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
-
+	
+	public static void SendUDP()
+	{
+		
+	}
+	
+	public static void SendTCP()
+	{
+		
+	}
+	
+	public static void SignalReceiver()
+	{
+		
+	}
+	
+	public static void FileBrowser()
+	{
+		
+	}
 }
